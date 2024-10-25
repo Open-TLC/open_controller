a596434d72ec8416afe8e22384f59324bd449d7b
Kari Koskinen <kari@conveqs.fi>
2024-10-25 13:18:41 +0300

    Changed the directory for clockwork, I will add the UI in a separate one

--- docker/clockwork/run_clockwork.sh
+++ docker/clockwork/run_clockwork.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This is used if we need local nats (for testing)
 #nats-server &
-python src/clockwork.py --conf-file=models/testmodel/oc_demo_full_features.json
+python clockwork/clockwork.py --conf-file=models/testmodel/oc_demo_full_features.json
