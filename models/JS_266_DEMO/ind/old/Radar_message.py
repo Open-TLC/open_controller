a6922d94a6aef8d8bc38fe32032c45a4102e7833
Iisakki Kosonen <iisakki@conveqs.fi>
2026-04-20 16:41:54 +0300

    JS266 DEMO clean

---
+++ models/JS_266_DEMO/ind/old/Radar_message.py
@@ -0,0 +1,328 @@
+
+{
+
+  "source": "radar.269.1.objects_port.json",
+
+  "status": "OK",
+
+  "tstamp": 1759999944328,
+
+  "nobjects": 12,
+
+  "objects": [
+
+    {
+
+      "id": 127,
+
+      "lat": 60.162226996520104,
+
+      "lon": 24.922633429294436,
+
+      "speed": 0.001,
+
+      "bearing": -147.36415,
+
+      "quality": 0.0,
+
+      "length": 5.6,
+
+      "class": 6,
+
+      "lane": 2,
+
+      "acceleration": 1.3367216e-10,
+
+      "last_updated": 634
+
+    },
+
+    {
+
+      "id": 112,
+
+      "lat": 60.16222371382317,
+
+      "lon": 24.922594200988005,
+
+      "speed": 0.001,
+
+      "bearing": -146.26936,
+
+      "quality": 0.0,
+
+      "length": 14.0,
+
+      "class": 8,
+
+      "lane": 2,
+
+      "acceleration": 2.7216712e-10,
+
+      "last_updated": 622
+
+    },
+
+    {
+
+      "id": 6,
+
+      "lat": 60.162240762542524,
+
+      "lon": 24.922555172954638,
+
+      "speed": 0.001,
+
+      "bearing": -147.01707,
+
+      "quality": 0.0,
+
+      "length": 4.6,
+
+      "class": 4,
+
+      "lane": 2,
+
+      "acceleration": -4.920656e-10,
+
+      "last_updated": 595
+
+    },
+
+    {
+
+      "id": 16,
+
+      "lat": 60.16226548580498,
+
+      "lon": 24.92258150433881,
+
+      "speed": 0.001,
+
+      "bearing": -146.63261,
+
+      "quality": 0.0,
+
+      "length": 4.6,
+
+      "class": 4,
+
+      "lane": 2,
+
+      "acceleration": -2.0450525e-10,
+
+      "last_updated": 550
+
+    },
+
+    {
+
+      "id": 126,
+
+      "lat": 60.1623329635864,
+
+      "lon": 24.922591371912162,
+
+      "speed": 0.001,
+
+      "bearing": -145.61473,
+
+      "quality": 0.0,
+
+      "length": 14.0,
+
+      "class": 8,
+
+      "lane": 0,
+
+      "acceleration": 3.4408512e-10,
+
+      "last_updated": 726
+
+    },
+
+    {
+
+      "id": 7,
+
+      "lat": 60.162404753759105,
+
+      "lon": 24.922559031044674,
+
+      "speed": 0.001,
+
+      "bearing": -145.44836,
+
+      "quality": 0.0,
+
+      "length": 5.4,
+
+      "class": 4,
+
+      "lane": 0,
+
+      "acceleration": -4.3647763e-10,
+
+      "last_updated": 687
+
+    },
+
+    {
+
+      "id": 1,
+
+      "lat": 60.16239721436927,
+
+      "lon": 24.922480109099325,
+
+      "speed": 0.001,
+
+      "bearing": -145.39041,
+
+      "quality": 0.0,
+
+      "length": 5.0,
+
+      "class": 4,
+
+      "lane": 1,
+
+      "acceleration": 4.2750048e-10,
+
+      "last_updated": 621
+
+    },
+
+    {
+
+      "id": 31,
+
+      "lat": 60.16245438678342,
+
+      "lon": 24.9224893547209,
+
+      "speed": 0.001,
+
+      "bearing": -145.30524,
+
+      "quality": 0.0,
+
+      "length": 4.6,
+
+      "class": 0,
+
+      "lane": 0,
+
+      "acceleration": -4.4386472e-10,
+
+      "last_updated": 64
+
+    },
+
+    {
+
+      "id": 28,
+
+      "lat": 60.16247122503762,
+
+      "lon": 24.9224650773214,
+
+      "speed": 0.20355119,
+
+      "bearing": -145.21234,
+
+      "quality": 0.16198258,
+
+      "length": 5.6,
+
+      "class": 6,
+
+      "lane": 0,
+
+      "acceleration": 0.00003127937,
+
+      "last_updated": 17
+
+    },
+
+    {
+
+      "id": 26,
+
+      "lat": 60.16245390503066,
+
+      "lon": 24.92240744448803,
+
+      "speed": 0.4174367,
+
+      "bearing": -145.24168,
+
+      "quality": 0.74562186,
+
+      "length": 5.0,
+
+      "class": 4,
+
+      "lane": 0,
+
+      "acceleration": -0.6148068,
+
+      "last_updated": 0
+
+    },
+
+    {
+
+      "id": 27,
+
+      "lat": 60.16252056108681,
+
+      "lon": 24.922333268635544,
+
+      "speed": 2.2125978,
+
+      "bearing": -145.06752,
+
+      "quality": 0.73622096,
+
+      "length": 4.6,
+
+      "class": 4,
+
+      "lane": 0,
+
+      "acceleration": -0.9387176,
+
+      "last_updated": 1
+
+    },
+
+    {
+
+      "id": 13,
+
+      "lat": 60.16262348909054,
+
+      "lon": 24.922095446568573,
+
+      "speed": 0.001,
+
+      "bearing": -144.88141,
+
+      "quality": 0.0,
+
+      "length": 4.6,
+
+      "class": 4,
+
+      "lane": 1,
+
+      "acceleration": 6.992673e-11,
+
+      "last_updated": 664
+
+    }
+
+  ]
+
+ 
