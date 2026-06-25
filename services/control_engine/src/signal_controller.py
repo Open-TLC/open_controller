69ffbb4374780e4231878e2f4ccc97c4ecb60af0
lattots <otso@lattu.biz>
2026-06-25 17:37:51 +0300

    feat: Unified signal controller API proposition...
    This is an initial proposition for a standard format for signal
    controllers in Open Controller. Standardizing the interaction with a
    controller allows for easier prototyping and integration of new
    controllers and control logics.
    
    This is not meant to be the final form of the API, and comments and
    ideas are welcome.

---
+++ services/control_engine/src/signal_controller.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class ControllerStatus:
+    def __init__(self, step_count: int, current_phase: str, next_phase: str) -> None:
+        self.step_count: int = step_count
+        self.current_phase: str = current_phase
+        self.next_phase: str = next_phase
+
+
+class SignalController(ABC):
+    """Controller interface for interacting with a signal controller.
+
+    All controllers should use signal groups internally to ensure adequate intergreen
+    times between conflicting greens. Signal controller's should be configured with
+    a JSON file in a standard format.
+    """
+
+    @abstractmethod
+    def tick(self) -> None:
+        """Advance the controller by one step.
+        This updates detections and signal states.
+        """
+        ...
+
+    @property
+    @abstractmethod
+    def status(self) -> ControllerStatus:
+        """Current controller status."""
+        ...
+
+    @property
+    @abstractmethod
+    def status_dict(self) -> dict[str, Any]:
+        """Current controller status as a dictionary."""
+        ...
+
+    @property
+    @abstractmethod
+    def signal_states(self) -> str:
+        """Signal states in Open Controller format."""
+        ...
+
+    @property
+    @abstractmethod
+    def signal_states_sumo(self) -> str:
+        """Signal states in SUMO format."""
+        ...
